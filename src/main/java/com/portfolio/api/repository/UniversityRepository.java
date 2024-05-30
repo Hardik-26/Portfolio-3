3f877ce6846a7f48a2bd75b953e67769203919b5
Hardik <hardik.2673@gmail.com>
2024-05-30 20:56:20 +0200

    Repository Added

--- src/main/java/com/portfolio/api/repository/UniversityRepository.java
+++ src/main/java/com/portfolio/api/repository/UniversityRepository.java
@@ -1,4 +1,9 @@
 package com.portfolio.api.repository;
 
-public class UniversityRepository {
+import com.portfolio.api.model.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UniversityRepository extends JpaRepository<University, Long>, JpaSpecificationExecutor<University> {
+
 }
