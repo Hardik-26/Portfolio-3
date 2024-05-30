5f3b2694d7c2ad333fc1b15a3a5d543c17a2d0e3
Hardik <hardik.2673@gmail.com>
2024-05-30 20:52:06 +0200

    Models Added

--- src/main/java/com/portfolio/api/model/Module.java
+++ src/main/java/com/portfolio/api/model/Module.java
@@ -1,4 +1,74 @@
 package com.portfolio.api.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Module {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int semester;
+    private int creditPoints;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    private University university;
+
+
+    // Constructors
+
+    public Module(Long id, String name, int semester, int creditPoints, University university) {
+        this.id = id;
+        this.name = name;
+        this.semester = semester;
+        this.creditPoints = creditPoints;
+        this.university = university;
+    }
+
+    public Module() {
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getCreditPoints() {
+        return creditPoints;
+    }
+
+    public void setCreditPoints(int creditPoints) {
+        this.creditPoints = creditPoints;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
 }
--- src/main/java/com/portfolio/api/model/University.java
+++ src/main/java/com/portfolio/api/model/University.java
@@ -1,4 +1,133 @@
 package com.portfolio.api.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String country;
+    private String department;
+    private String url;
+    private String contactPerson;
+    private int studentsSent;
+    private int studentsAccepted;
+    private LocalDate springSemesterStart;
+    private LocalDate autumnSemesterStart;
+    @OneToMany(mappedBy = "university", cascade = CascadeType.ALL)
+    private List<Module> modules;
+
+    // Constructors-
+    public University(Long id, String name, String country, String department, String url, String contactPerson, int studentsSent, int studentsAccepted, LocalDate springSemesterStart, LocalDate autumnSemesterStart, List<Module> modules) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.department = department;
+        this.url = url;
+        this.contactPerson = contactPerson;
+        this.studentsSent = studentsSent;
+        this.studentsAccepted = studentsAccepted;
+        this.springSemesterStart = springSemesterStart;
+        this.autumnSemesterStart = autumnSemesterStart;
+        this.modules = modules;
+    }
+
+    public University() {
+    }
+
+    // Getters and Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public int getStudentsSent() {
+        return studentsSent;
+    }
+
+    public void setStudentsSent(int studentsSent) {
+        this.studentsSent = studentsSent;
+    }
+
+    public int getStudentsAccepted() {
+        return studentsAccepted;
+    }
+
+    public void setStudentsAccepted(int studentsAccepted) {
+        this.studentsAccepted = studentsAccepted;
+    }
+
+    public LocalDate getSpringSemesterStart() {
+        return springSemesterStart;
+    }
+
+    public void setSpringSemesterStart(LocalDate springSemesterStart) {
+        this.springSemesterStart = springSemesterStart;
+    }
+
+    public LocalDate getAutumnSemesterStart() {
+        return autumnSemesterStart;
+    }
+
+    public void setAutumnSemesterStart(LocalDate autumnSemesterStart) {
+        this.autumnSemesterStart = autumnSemesterStart;
+    }
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+    }
 }
