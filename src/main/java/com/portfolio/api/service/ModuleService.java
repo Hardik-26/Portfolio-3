68764e5b1efcee346b0ef3683e524f79df8a0f73
Hardik <hardik.2673@gmail.com>
2024-05-31 17:22:09 +0200

    Repository Added

--- src/main/java/com/portfolio/api/service/ModuleService.java
+++ src/main/java/com/portfolio/api/service/ModuleService.java
@@ -1,4 +1,32 @@
 package com.portfolio.api.service;
 
+import com.portfolio.api.model.Module;
+import com.portfolio.api.repository.ModuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ModuleService {
+
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    public Module createModule(Module module) {
+        return moduleRepository.save(module);
+    }
+
+    public Optional<Module> getModule(Long id) {
+        return moduleRepository.findById(id);
+    }
+
+    public Module updateModule(Long id, Module module) {
+        module.setId(id);
+        return moduleRepository.save(module);
+    }
+
+    public void deleteModule(Long id) {
+        moduleRepository.deleteById(id);
+    }
 }
--- src/main/java/com/portfolio/api/service/UniversityService.java
+++ src/main/java/com/portfolio/api/service/UniversityService.java
@@ -1,4 +1,40 @@
 package com.portfolio.api.service;
 
+
+import com.portfolio.api.model.University;
+import com.portfolio.api.repository.UniversityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UniversityService {
+
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    public University createUniversity(University university) {
+        return universityRepository.save(university);
+    }
+
+    public Optional<University> getUniversity(Long id) {
+        return universityRepository.findById(id);
+    }
+
+    public University updateUniversity(Long id, University university) {
+        university.setId(id);
+        return universityRepository.save(university);
+    }
+
+    public void deleteUniversity(Long id) {
+        universityRepository.deleteById(id);
+    }
+
+    public Page<University> searchUniversities(Specification<University> spec, Pageable pageable) {
+        return universityRepository.findAll(spec, pageable);
+    }
 }
